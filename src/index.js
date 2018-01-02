0f22af459b95a001922ae870a21c5eb40fbf5343
Adam Peryman <adam.peryman@gmail.com>
2018-01-02 23:51:39 +1300

    Setup domain with dummy User model.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { model, Schema } from 'mongoose'
+
+export const UserSchema = new Schema({
+  _id: {
+    type: Schema.types.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+export const User = model('User', UserSchema)
